0fc46a5054766f02862e3b16894e3cef6ac8d4c4
PriyamAgarwal999 <40921523+PriyamAgarwal999@users.noreply.github.com>
2022-12-29 11:59:47 +0530

    Add files via upload

---
+++ newProject/src/main/java/com/example/demo/details/Student.java
@@ -0,0 +1,52 @@
+package com.example.demo.details;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity
+public class Student {
+
+    @Id
+    @JsonProperty("Id")
+    public int id;
+
+    @JsonProperty("First_Name")
+    public String firstName;
+
+    @JsonProperty("Last_Name")
+    public String lastName;
+
+    @JsonProperty("DOB")
+    public String dateOfBirth;
+
+    @JsonProperty("age")
+    public int age;
+
+    @JsonProperty("Section")
+    public String section;
+
+    @JsonProperty("Gender")
+    public String gender;
+
+    @JsonProperty("Marks1")
+    public int marks1;
+
+    @JsonProperty("Marks2")
+    public int marks2;
+
+    @JsonProperty("Marks3")
+    public int marks3;
+
+    @JsonProperty("Total")
+    public int total;
+
+    @JsonProperty("Average")
+    public float average;
+
+    @JsonProperty("Result")
+    public String result;
+    
+}
